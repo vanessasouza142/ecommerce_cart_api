4e440a1abf7f75c5e34d6dc6ddee25f513e1e843
Vanessa Souza <vanessa.kbs16@gmail.com>
2026-02-07 19:07:17 -0300

    Add callbacks to update cart total_price after save and destroy

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -4,7 +4,16 @@ class CartProduct < ApplicationRecord
   belongs_to :cart
   belongs_to :product
 
+  after_save :update_cart_total_price
+  after_destroy :update_cart_total_price
+
   def total_price
     product.price * quantity
+  end
+
+  private
+
+  def update_cart_total_price
+    cart.update!(total_price: cart.total_price)
   end
 end
