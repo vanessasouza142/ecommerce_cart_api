083474938b040dfd2715619ee588c9d59c3babe5
Vanessa Souza <vanessa.kbs16@gmail.com>
2026-02-08 12:42:30 -0300

    Add cart_product model tests

--- spec/models/cart_product_spec.rb
+++ spec/models/cart_product_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe CartProduct, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when validating' do
+    it 'validates numericality of quantity' do
+      cart_product = described_class.new(quantity: -1)
+      expect(cart_product.valid?).to be_falsey
+      expect(cart_product.errors[:quantity]).to include("must be greater than 0")
+    end
+
+    it 'validates numericality of quantity' do
+      cart_product = described_class.new(quantity: "a")
+      expect(cart_product.valid?).to be_falsey
+      expect(cart_product.errors[:quantity]).to include("is not a number")
+    end
+  end
 end
