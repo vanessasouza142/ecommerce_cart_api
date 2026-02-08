0ea4a169abda723d64b8ff0148fef6f7119fc0eb
Vanessa Souza <vanessa.kbs16@gmail.com>
2026-02-08 12:12:20 -0300

    Add carts routing tests

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   resources :products
   resource :cart, only: [:show, :create ] do
-    patch '/add_item', to: 'carts#add_item'
+    post '/add_item', to: 'carts#add_item'
     delete '/:product_id', to: 'carts#destroy'
   end
   get "up" => "rails/health#show", as: :rails_health_check
